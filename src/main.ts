62a16816ea228e9f605c7904a41f51b9b83d8d05
Narotam-Mishra <narotam21jan@gmail.com>
2024-01-08 00:25:51 +0530

    todo-app-ts_1.2

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ const myForm = document.getElementById("myForm") as HTMLFormElement;
 myForm.onsubmit = (e: SubmitEvent) => {
   e.preventDefault();
 
-  const todo:Todo = {
+  const todo: Todo = {
     title: todoInput.value,
     isCompleted: false,
     id: String(Math.random() * 1000)
@@ -32,37 +32,56 @@ myForm.onsubmit = (e: SubmitEvent) => {
 }
 
 const generateTodoItem = (title:string, isCompleted:boolean, id:string) => {
-  const todo:HTMLDivElement = document.createElement("div");
+  const todo: HTMLDivElement = document.createElement("div");
   todo.className = "todo";
 
   // Creating a checkbox
-  const checkBox:HTMLInputElement = document.createElement("input");
+  const checkBox: HTMLInputElement = document.createElement("input");
   checkBox.setAttribute("type", "checkbox");
   checkBox.className = "isCompleted";
   checkBox.checked = isCompleted;
   checkBox.onchange = () => {
-    
-  }
+
+    todos.find(item => {
+      if(item.id === id){
+        item.isCompleted = checkBox.checked;
+      }
+    })
+    // console.log(isCompleted);
+    // console.log(checkBox.checked);
+    paraElement.className = checkBox.checked ? "textCut" : "";   
+  };
 
   // Creating P for title
   const paraElement:HTMLParagraphElement = document.createElement("p");
   paraElement.innerText = title;
+  paraElement.className = isCompleted ? "textCut" : "";   
 
   // Creating delete button
   const btn:HTMLButtonElement = document.createElement("button");
-  btn.innerText = "❌"
+  btn.innerText = "X"
   btn.className = "deleteBtn";
+  btn.onclick = () => {
+    deleteTodo(id);
+  }
 
   // appending all to TodoItem
   todo.append(checkBox, paraElement, btn);
-
   todosContainer.append(todo);
 
 }
 
-const renderTodo = (todos:Todo[]) => {
+// delete todo item
+const deleteTodo = (id:string) => {
+  const idx = todos.findIndex(item => item.id === id);
+  todos.splice(idx,1);
+  renderTodo(todos);
+}
+
+// render todo list
+const renderTodo = (todos: Todo[]) => {
   todosContainer.innerText = "";
   todos.forEach(item => {
     generateTodoItem(item.title, item.isCompleted, item.id);
-  })
+  });
 }
